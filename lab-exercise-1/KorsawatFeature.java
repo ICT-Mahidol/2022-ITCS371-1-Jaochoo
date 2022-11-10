e711a989c8ad836a5f4813b72933c808f485ba6b
Korsawat Baiseejuang <korsawat.bai@student.mahidol.edu>
2022-11-10 19:31:49 +0700

    Fixed the feature

--- lab-exercise-1/KorsawatFeature.java
+++ lab-exercise-1/KorsawatFeature.java
@@ -1,5 +1,6 @@
-  public class KorsawatFeature {
-      public void feature() {
+      public void Korsawatfeature() {
           // this is a new feature
+          int x = 0;
+          x = x+1;
       }
   }
